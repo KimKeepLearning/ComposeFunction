49f332dd89113c68b8ed84dc6f2e876ab5657588
Y. Huang <18261195537@163.com>
2024-05-06 13:56:34 +0800

    feat: 认识reduce

---
+++ reduce.js
@@ -0,0 +1,28 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]  
+
+
+function pipe(funcs) {
+  function callback(prevInput, curFunc) {
+    return curFunc(prevInput);
+  }
+  return function (param) {
+    return funcs.reduce(callback, param);
+  }
+}
+
+function add4(num) {
+  return num + 4
+}  
+
+function multiply3(num) {
+  return num*3
+}  
+
+function divide2(num) {
+  return num/2
+}
+
+const compute = pipe([add4, multiply3, divide2])
+
+console.log(compute(10));
+
